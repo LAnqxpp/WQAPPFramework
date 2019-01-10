e1143eec8284f502b69b74df0220273f06caeed3
LAnqxpp <azir_likes@126.com>
2019-01-10 11:16:01 +0800

    1、添加一个项目

---
+++ BaseAPPFramework/BaseAPPFramework/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  BaseAPPFramework
+//
+//  Created by mac on 2019/1/10.
+//  Copyright © 2019年 Robinson. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
