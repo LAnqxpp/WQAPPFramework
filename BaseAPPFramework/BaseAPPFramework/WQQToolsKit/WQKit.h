f817f960466fafda95166147ef1b484a526743b2
LAnqxpp <azir_likes@126.com>
2019-01-10 13:44:16 +0800

    添加Kit文件夹

---
+++ BaseAPPFramework/BaseAPPFramework/WQQToolsKit/WQKit.h
@@ -0,0 +1,17 @@
+//
+//  WQKit.h
+//  BaseAPPFramework
+//
+//  Created by mac on 2019/1/10.
+//  Copyright © 2019年 Robinson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WQKit : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ BaseAPPFramework/BaseAPPFramework/WQQToolsKit/WQKit.m
@@ -0,0 +1,13 @@
+//
+//  WQKit.m
+//  BaseAPPFramework
+//
+//  Created by mac on 2019/1/10.
+//  Copyright © 2019年 Robinson. All rights reserved.
+//
+
+#import "WQKit.h"
+
+@implementation WQKit
+
+@end
