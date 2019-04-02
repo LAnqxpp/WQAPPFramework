143548f7774fd234369f4a6dc449c6713e9b23cc
王383160955 <azir_likes@126.com>
2019-04-02 14:55:39 +0800

    no message

---
+++ BaseAPPFramework/BaseAPPFramework/WQQToolsKit/当前显示控制器名称打印2选1/UIViewController+WQSwizzling.h
@@ -0,0 +1,17 @@
+//
+//  UIViewController+WQSwizzling.h
+//  KuaiLiao
+//
+//  Created by mac on 2019/4/2.
+//  Copyright © 2019 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIViewController (WQSwizzling)
+
+@end
+
+NS_ASSUME_NONNULL_END
